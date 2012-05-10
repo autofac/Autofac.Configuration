31e633b84c5f4e4ccb160c83a1029784024b4fda
travis.illig@gmail.com <unknown@kiln.example.com>
2012-05-10 22:02:17 +0700

    Moved core test assemblies to new location.

---
+++ AssertionHelpers.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Autofac.Core;
+
+namespace Autofac.Tests.Configuration
+{
+    static class AssertionHelpers
+    {
+        public static void AssertRegistered<TService>(this IComponentContext context)
+        {
+            Assert.IsTrue(context.IsRegistered<TService>());
+        }
+
+        public static void AssertNotRegistered<TService>(this IComponentContext context)
+        {
+            Assert.IsFalse(context.IsRegistered<TService>());
+        }
+
+        public static void AssertRegistered<TService>(this IComponentContext context, string service)
+        {
+            Assert.IsTrue(context.IsRegisteredWithName(service, typeof(TService)));
+        }
+
+        public static void AssertNotRegistered<TService>(this IComponentContext context, string service)
+        {
+            Assert.IsFalse(context.IsRegisteredWithName(service, typeof(TService)));
+        }
+    }
+}
---
+++ ConfigurationSettingsReaderFixture.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac.Builder;
+using Autofac.Configuration;
+using NUnit.Framework;
+using Autofac.Core;
+
+namespace Autofac.Tests.Configuration
+{
+    [TestFixture]
+    public class ConfigurationSettingsReaderFixture
+    {
+        [Test]
+        public void ReadsMetadata()
+        {
+            var container = ConfigureContainer("Metadata").Build();
+            IComponentRegistration registration;
+            Assert.IsTrue(container.ComponentRegistry.TryGetRegistration(new KeyedService("a", typeof(object)), out registration));
+            Assert.AreEqual(42, (int)registration.Metadata["answer"]);
+        }
+
+        [Test]
+        public void IncludesFileReferences()
+        {
+            var container = ConfigureContainer("Referrer").Build();
+            container.AssertRegistered<object>("a");
+        }
+
+        interface IA { }
+
+        class A : IA
+        {
+            public A(int i) { I = i; }
+
+            public int I { get; set; }
+
+            public string Message { get; set; }
+        }
+
+        [Test]
+        public void SingletonWithTwoServices()
+        {
+            var container = ConfigureContainer("SingletonWithTwoServices").Build();
+            container.AssertRegistered<IA>();
+            container.AssertRegistered<object>();
+            container.AssertNotRegistered<A>();
+            Assert.AreSame(container.Resolve<IA>(), container.Resolve<object>());
+        }
+
+        [Test]
+        public void ParametersProvided()
+        {
+            var container = ConfigureContainer("SingletonWithTwoServices").Build();
+            var cpt = (A)container.Resolve<IA>();
+            Assert.AreEqual(1, cpt.I);
+        }
+
+        [Test]
+        public void PropertiesProvided()
+        {
+            var container = ConfigureContainer("SingletonWithTwoServices").Build();
+            var cpt = (A)container.Resolve<IA>();
+            Assert.AreEqual("hello", cpt.Message);
+        }
+
+        class B { }
+
+        [Test]
+        public void FactoryScope()
+        {
+            var container = ConfigureContainer("BPerDependency").Build();
+            Assert.AreNotSame(container.Resolve<B>(), container.Resolve<B>());
+        }
+
+        class C
+        {
+            public bool ABool { get; set; }
+        }
+
+        [Test]
+        public void ConfiguresBooleanProperties()
+        {
+            var container = ConfigureContainer("CWithBoolean").Build();
+            var c = container.Resolve<C>();
+            Assert.IsTrue(c.ABool);
+        }
+
+        [Test]
+        public void SetsExternalOwnership()
+        {
+            var container = ConfigureContainer("BExternal").Build();
+            IComponentRegistration forB;
+            container.ComponentRegistry.TryGetRegistration(new TypedService(typeof(B)), out forB);
+            Assert.AreEqual(InstanceOwnership.ExternallyOwned, forB.Ownership);
+        }
+
+        class E
+        {
+            public B B { get; set; }
+        }
+
+        [Test]
+        public void SetsPropertyInjection()
+        {
+            var builder = ConfigureContainer("EWithPropertyInjection");
+            builder.RegisterType<B>();
+            var container = builder.Build();
+            var e = container.Resolve<E>();
+            Assert.IsNotNull(e.B);
+        }
+
+        class D : IA { }
+
+        [Test]
+        public void ConfiguresMemberOf()
+        {
+            var builder = ConfigureContainer("MemberOf");
+            builder.RegisterCollection<IA>("ia")
+                    .As<IList<IA>>();
+            var container = builder.Build();
+            var collection = container.Resolve<IList<IA>>();
+            var first = collection[0];
+            Assert.IsInstanceOf(typeof(D), first);
+        }
+
+        class X 
+        {
+            public string Name { get; set; }
+            public X(string name) { Name = name; } 
+        }
+
+        [Test]
+        public void AllowsMultipleRegistrationsOfSameType()
+        {
+            var builder = ConfigureContainer("TypeMultipleTimes");
+            var container = builder.Build();
+            var collection = container.Resolve<IEnumerable<X>>();
+            Assert.AreEqual(2, collection.Count());
+            Assert.AreEqual("Bar", collection.ElementAt(0).Name);
+            Assert.AreEqual("Foo", collection.ElementAt(1).Name);
+        }
+
+        static ContainerBuilder ConfigureContainer(string configFile)
+        {
+            var cb = new ContainerBuilder();
+            var fullFilename = configFile + ".config";
+            var csr = new ConfigurationSettingsReader(ConfigurationSettingsReader.DefaultSectionName, fullFilename);
+            cb.RegisterModule(csr);
+            return cb;
+        }
+    }
+}
