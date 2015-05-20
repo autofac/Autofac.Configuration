d28fa54ea5c8b601b61e7455950271e145a62499
Travis Illig <tillig@paraesthesia.com>
2015-05-20 09:22:51 -0700

    Updated build to use Powershell.

---
+++ build.ps1
@@ -0,0 +1,33 @@
+# Build variables
+$dnvmVersion = "1.0.0-beta4";
+
+Push-Location $PSScriptRoot
+
+# Clean
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+# Install DNVM
+if(((where.exe dnvm) | Out-String) -eq "")
+{
+    &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
+}
+
+# Install DNX
+dnvm install $dnvmVersion -r CoreCLR
+dnvm install $dnvmVersion -r CLR
+dnvm use $dnvmVersion -r CLR
+
+# Package restore
+Get-ChildItem -Path . -Filter *.xproj -Recurse | ForEach-Object { dnu restore ("""" + $_.DirectoryName + """") }
+
+# Set build number
+$env:DNX_BUILD_VERSION = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1}[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+Write-Host "Build number:" $env:DNX_BUILD_VERSION
+
+# Build/package
+Get-ChildItem -Path .\src -Filter *.xproj -Recurse | ForEach-Object { dnu pack ("""" + $_.DirectoryName + """") --configuration Release --out .\artifacts\packages; if($LASTEXITCODE -ne 0) { throw "Build failed on "  + $_.DirectoryName } }
+
+# Test
+Get-ChildItem -Path .\test -Filter *.xproj -Recurse | ForEach-Object { dnx ("""" + $_.DirectoryName + """") test; if($LASTEXITCODE -ne 0) { throw "Tests failed on "  + $_.DirectoryName } }
+
+Pop-Location
